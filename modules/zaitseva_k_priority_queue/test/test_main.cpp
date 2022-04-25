3d7ac55bb11e8dcdd7511be209f25321ec69c6d8
Kcenia <Kcenia4010@gmail.com>
2022-04-25 22:25:06 +0300

    initial commit

---
+++ modules/zaitseva_k_priority_queue/test/test_main.cpp
@@ -0,0 +1,9 @@
+// Copyright 2022 Zaitseva Ksenia
+#include "include/priority_queue.h"
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
